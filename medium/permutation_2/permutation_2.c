551ef49cd0ec4f9d391d94c1038b7f8c9e2ba56e
peter chiang <peterstoneage2@gmail.com>
2017-05-07 05:57:46 -0700

    permutation 2

--- medium/permutation_2/permutation_2.c
+++ medium/permutation_2/permutation_2.c
@@ -3,39 +3,28 @@
 #include <string.h>
 
 int** permute(int* nums, int numsSize, int* returnSize);
-void DFS(int depth, int *p, int numsSize, int *ele, int **r, int *returnSize);
-int comp(const void *a, const void *b);
+void DFS(int depth, int *p, int numsSize, int *ele, int **r);
 
 int visit[100] = {0};
 int count = 0;
 
 int main(void)
 {
-    int a[4] = { 1,7,7,11 };
+    int a[4] = { 1,3,7,11 };
     int size,i,j;
     int ** b= permute(a,4,&size);
 
 }
 
-int comp(const void *a, const void *b)
-{
-    int c = *(int*)a;
-    int d = *(int*)b;
-
-    return (c > d)? 1 : (c < d)? -1 : 0;
-}
 
 int** permute(int* nums, int numsSize, int* returnSize)
 {
     int **r;
     int i = numsSize;
-    int j = 0;
     *returnSize =1;
     int p[numsSize];
-    int dependence[numsSize];
     memset(visit,0,sizeof(int)*100);
     count = 0;
-    qsort((void*)nums,numsSize,sizeof(int),comp);
     while (i)
     {
         (*returnSize) *= i--;
@@ -45,12 +34,45 @@ int** permute(int* nums, int numsSize, int* returnSize)
     {
         r[i] = (int *)malloc(sizeof(int) * numsSize );
     }
-    *returnSize = 0;
-    DFS(0,p,numsSize,nums,r,returnSize);
+
+    DFS(0,p,numsSize,nums,r);
 
     return r;
 }
-void DFS(int depth,int *p, int numsSize, int *ele,int **r, int *returnSize)
+
+void DFS(int depth,int *p, int numsSize, int *ele,int **r)
+{
+    unsigned int i ;
+    if (depth < numsSize)
+    {
+        for ( i = 0 ; i < numsSize ; i ++ )
+        {
+            if (!visit[i])
+            {
+                if ( ele[i] == ele[i-1] && visit[i-1] && i>0 ) return ;
+                //printf("depth :%d , visit :%d\n",depth,ele[i]);
+                visit[i] = 1;
+                p[depth] = ele[i];
+                DFS(depth+1,p,numsSize,ele,r);
+                visit[i] = 0;
+            }
+        }
+    
+    }
+
+    else {
+        for (i = 0 ; i < numsSize ; i ++ )
+        {
+            printf("%d ",p[i]);
+
+        }
+        printf("\n");
+        memcpy(r[count++],p,sizeof(int)*numsSize);
+    
+    }
+}
+
+/*void DFS(int depth,int *p, int numsSize, int *ele,int **r)
 {
     unsigned int i ;
     unsigned int temp;
@@ -59,22 +81,24 @@ void DFS(int depth,int *p, int numsSize, int *ele,int **r, int *returnSize)
         for ( i = depth ; i < numsSize ; i ++ )
         {
                 //printf("depth :%d , visit :%d\n",depth,ele[i]);
-            if ( ele[depth] == ele[i] && depth != i ) continue;
             temp = ele[depth];
             ele[depth] = ele[i];
             ele[i] = temp;
             p[depth] = ele[depth];
-            DFS(depth+1,p,numsSize,ele,r, returnSize);
+            DFS(depth+1,p,numsSize,ele,r);
+//            temp = ele[depth];
+//            ele[depth] = ele[i];
+//            ele[i] = temp;
         }
     
     }
 
     else {
-        memcpy(r[(*returnSize)++],p,sizeof(int)*numsSize);
+        memcpy(r[count++],p,sizeof(int)*numsSize);
         for ( i = 0 ; i < numsSize ; i ++ )
         {
             printf("%d ",p[i]);
         }
         printf("\n");
     }
-}
+}*/
